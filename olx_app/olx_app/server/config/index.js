ccef510cd10135420093f38557ed0762ffb96cef
MudassirRaza12345 <mudassirraza12345@gmail.com>
2020-10-28 03:39:27 -0700

    firstcommit

--- olx_app/olx_app/server/config/index.js
+++
@@ -1,8 +0,0 @@
-const configValues = require('./config');
-
-module.exports = {
-
-    getDB_Connection_URL : () => {
-    return `mongodb://${ configValues.user_name }:${configValues.password}@ds125502.mlab.com:25502/server-mongodb`;
-    }
-}
